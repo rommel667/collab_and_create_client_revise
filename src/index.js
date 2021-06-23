8f99a039ab0b9155f25ab327c33f7f8a71e31bfe
rommel667 <rommel667@gmail.com>
2021-06-23 18:42:48 +0800

    setup graphql apollo client

--- src/index.js
+++ src/index.js
@@ -4,12 +4,56 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink } from "@apollo/client"
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { setContext } from 'apollo-link-context'
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+})
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true
+  }
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const authorizationLink = setContext(() => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authorizationLink.concat(splitLink),
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>
   ,
   document.getElementById('root')
 );
