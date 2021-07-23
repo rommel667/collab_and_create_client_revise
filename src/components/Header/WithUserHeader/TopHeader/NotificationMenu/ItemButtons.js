60e28e8546189c92e9c7e0fdd66be4341068af22
rommel667 <rommel667@gmail.com>
2021-07-23 19:35:51 +0800

    fix issue of disappering newly accepted projects after other user accepted same project

--- src/components/Header/WithUserHeader/TopHeader/NotificationMenu/ItemButtons.js
+++ src/components/Header/WithUserHeader/TopHeader/NotificationMenu/ItemButtons.js
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { ACCEPT_PROJECT_INVITE, REJECT_PROJECT_INVITE } from '../../../../../graphql/gql/notification/mutation'
+import { PROJECTS_BY_USER } from '../../../../../graphql/queries/project/ProjectsByUserQuery'
 
 const ItemButtons = ({ acceptedInvites, rejectedInvites, projectId }) => {
 
@@ -9,7 +10,21 @@ const ItemButtons = ({ acceptedInvites, rejectedInvites, projectId }) => {
 
     const [acceptProjectInvite] = useMutation(ACCEPT_PROJECT_INVITE, {
         update(proxy, result) {
-            dispatch({ type: "ACCEPT_PROJECT_INVITE", payload: { project: result.data.acceptProjectInvite } })
+            const data = proxy.readQuery({
+                query: PROJECTS_BY_USER
+            })
+            if (data) {
+                const newData = [ ...data.projectsByUser, result.data.acceptProjectInvite ]
+                proxy.writeQuery({
+                    query: PROJECTS_BY_USER,
+                    data: {
+                        projectsByUser: [...newData]
+                    }
+                })
+                dispatch({ type: "PROJECTS_BY_USER", payload: { projects: [...newData] } })
+                dispatch({ type: "ACCEPT_PROJECT_INVITE", payload: { project: result.data.acceptProjectInvite } })
+            }
+            
         },
         variables: {
             projectId
@@ -27,7 +42,7 @@ const ItemButtons = ({ acceptedInvites, rejectedInvites, projectId }) => {
 
     return (
         <>
-        {acceptedInvites.includes(projectId) || rejectedInvites.includes(projectId) === false ?
+        {(acceptedInvites.includes(projectId) || rejectedInvites.includes(projectId)) === false ?
             <div className="flex gap-1 mt-3">
                 <button
                     onClick={acceptProjectInvite}
--- src/redux/reducers/projectReducer.js
+++ src/redux/reducers/projectReducer.js
@@ -30,18 +30,18 @@ const project = (state = initialState, action) => {
                 projects: [ ...sortedProjects ]
             }
         }
-        case "ACCEPT_PROJECT_INVITE": {
-            const projectsClone = [...state.projects, action.payload.project]
-            const sortedProjects = projectsClone.sort((a, b) => {
-                const d1 = new Date(a.createdAt)
-                const d2 = new Date(b.createdAt)
-                return d1.getTime() - d2.getTime()
-            })
-            return {
-                ...state,
-                projects: [ ...sortedProjects ]
-            }
-        }
+        // case "ACCEPT_PROJECT_INVITE": {
+        //     const projectsClone = [...state.projects, action.payload.project]
+        //     const sortedProjects = projectsClone.sort((a, b) => {
+        //         const d1 = new Date(a.createdAt)
+        //         const d2 = new Date(b.createdAt)
+        //         return d1.getTime() - d2.getTime()
+        //     })
+        //     return {
+        //         ...state,
+        //         projects: [ ...sortedProjects ]
+        //     }
+        // }
         case "ACCEPT_PROJECT_INVITE_SUBSCRIPTION": {
             return {
                 ...state,
